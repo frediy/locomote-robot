5635294930151ecd277ff0ce59956af5bb7f1dad
Fredrik Persen Fostvedt <fpfostvedt@gmail.com>
2017-08-08 19:33:24 +1000

    initial

---
+++ lib/locomote_robot.rb
@@ -0,0 +1,5 @@
+require "locomote_robot/version"
+
+module LocomoteRobot
+  # Your code goes here...
+end
---
+++ spec/locomote_robot_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+RSpec.describe LocomoteRobot do
+  it "has a version number" do
+    expect(LocomoteRobot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+  end
+end
